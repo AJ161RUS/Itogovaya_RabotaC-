dd1cf7a965edaa0bcf120d084229d7536cb96d84
Andrey Zhulay <zhulay.00@mail.ru>
2023-04-24 20:11:40 +0300

    Написание программы

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write($"Введите масив через пробел: ");
+string[] array1 =  Console.ReadLine()!.Split(' ', ' ', ' ', '.');
+string[] array2 = new string[array1.Length];
+
+void Array1(string[] array1, string[] array2)
+{
+    int count = 0;
+    int m = 3;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= m)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}   
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.Write($"{array[array.Length - 1]}");
+}
+    int m = 3;
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= m)
+        {
+        count++;
+        }
+    }
+Array.Resize(ref array2, count);
+
+try
+{ Array1(array1, array2);
+Console.Write("[");
+PrintArray(array2);
+}
+catch
+{
+    Console.Write($"");
+}
+Console.WriteLine("]");
